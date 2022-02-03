7fb7a30be2f16e5576f86185137a3809a8b6aba5
Christopher324 <98764303+Christopher324@users.noreply.github.com>
2022-02-03 10:17:53 -0600

    Add files via upload

---
+++ StringsDemo.java
@@ -0,0 +1,17 @@
+
+public class StringsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Christopher";
+		String middleName = "S";
+	    String lastName = "Reyes";
+	    String dateofBirth = "01/01/01";
+	    System.out.println("First Name: " + firstName);
+	    System.out.println("Middle Name: " + middleName);
+	    System.out.println("Last Name: " + lastName);
+	    System.out.println("DOB: " + dateofBirth);
+	   
+	}
+
+}
